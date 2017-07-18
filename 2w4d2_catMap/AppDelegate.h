abe5adc4a65d1b6c28d8cfff21ba6c5536547c37
hkimca <harrison.kim.ca@gmail.com>
2017-07-18 15:32:32 -0400

    Initial Commit

---
+++ 2w4d2_catMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2w4d2_catMap
+//
+//  Created by Seantastic31 on 18/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
